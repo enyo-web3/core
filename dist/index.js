43d453320093e712c8af271f149a87934e5111ea
Carlos Diaz-Padron <carlos@metaphor.xyz>
2022-04-11 01:24:52 -0400

    use commonjs

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,9 @@
-import { SchemaLink } from '@apollo/client/link/schema';
-import { mergeTypeDefs } from '@graphql-tools/merge';
-import { mergeSchemas } from '@graphql-tools/schema';
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.EnyoSupergraph = void 0;
+const schema_1 = require("@apollo/client/link/schema");
+const merge_1 = require("@graphql-tools/merge");
+const schema_2 = require("@graphql-tools/schema");
 // type UnionToIntersection<T> = (T extends T ? (p: T) => void : never) extends (p: infer U) => void ? U : never;
 // type SubgraphProviders<T extends ReadonlyArray<EnyoSubgraph<EnyoProvider>>> = {
 //   [K in keyof T]: T[K] extends EnyoSubgraph<infer V> ? V : never;
@@ -12,20 +15,21 @@ import { mergeSchemas } from '@graphql-tools/schema';
 // > = UnionToIntersection<T[K]>;
 // type RequiredProviders<T extends ReadonlyArray<EnyoSubgraph<EnyoProvider>>> =
 //   SubgraphProviders<T>[keyof SubgraphProviders<T>];
-export class EnyoSupergraph {
+class EnyoSupergraph {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     constructor(options) {
         this.providers = options.providers;
         this.subgraphs = options.subgraphs;
     }
     link() {
-        const schema = mergeSchemas({ schemas: this.subgraphs.map(s => s.schema(this.providers)) });
-        return new SchemaLink({ schema });
+        const schema = (0, schema_2.mergeSchemas)({ schemas: this.subgraphs.map(s => s.schema(this.providers)) });
+        return new schema_1.SchemaLink({ schema });
     }
     typeDefs(options) {
         return options && options.extraTypeDefs
-            ? mergeTypeDefs([...this.subgraphs.map(s => s.typeDefs()), options.extraTypeDefs])
-            : mergeTypeDefs(this.subgraphs.map(s => s.typeDefs()));
+            ? (0, merge_1.mergeTypeDefs)([...this.subgraphs.map(s => s.typeDefs()), options.extraTypeDefs])
+            : (0, merge_1.mergeTypeDefs)(this.subgraphs.map(s => s.typeDefs()));
     }
 }
+exports.EnyoSupergraph = EnyoSupergraph;
 //# sourceMappingURL=index.js.map
